70a9eb5f4a702dc644b1fcf5a30f5f8ee9cb256a
Zuhair <zuhair.gulzar@originbluy.com>
2024-02-01 08:05:35 -0500

    Server Update Functionality Done

---
+++ app/api/servers/[serverId]/route.ts
@@ -0,0 +1,35 @@
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { NextResponse } from "next/server";
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { serverId: string } }
+) {
+  try {
+    let profile = await currentProfile();
+    let { name, imageUrl } = await req.json();
+
+    console.log("REQUEST", req.json());
+
+    if (!profile) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const server = await db.server.update({
+      where: {
+        id: params.serverId,
+        profileId: profile.id,
+      },
+      data: {
+        name,
+        imageUrl,
+      },
+    });
+
+    return NextResponse.json(server);
+  } catch (error) {
+    console.log("Edit Server Error", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
